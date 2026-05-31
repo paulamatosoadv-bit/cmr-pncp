ebe2be4cbe6b44ede6297423526618cae992bdeb
paulamatosoadv-bit <paulamatosoadv@gmail.com>
2026-05-31 19:51:39 -0300

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,16 +5,18 @@ exports.handler = async function(event) {
   const seq = qs.seq || '';
   const endpoint = qs.endpoint || 'contratacao';
   const numeroItem = qs.numeroItem || '';
-  const BASE = 'https://pncp.gov.br/api/consulta/v1';
+
   let url;
   if (endpoint === 'contratacao') {
-    url = `${BASE}/orgaos/${cnpj}/compras/${ano}/${seq}`;
+    url = `https://pncp.gov.br/api/consulta/v1/orgaos/${cnpj}/compras/${ano}/${seq}`;
   } else if (endpoint === 'itens') {
-    url = `${BASE}/orgaos/${cnpj}/compras/${ano}/${seq}/itens?pagina=1&tamanhoPagina=500`;
+    url = `https://pncp.gov.br/api/pncp/v1/orgaos/${cnpj}/compras/${ano}/${seq}/itens?pagina=1&tamanhoPagina=500`;
   } else if (endpoint === 'resultados') {
-    url = `${BASE}/orgaos/${cnpj}/compras/${ano}/${seq}/itens/${numeroItem}/resultados?pagina=1&tamanhoPagina=500`;
+    url = `https://pncp.gov.br/api/pncp/v1/orgaos/${cnpj}/compras/${ano}/${seq}/itens/${numeroItem}/resultados?pagina=1&tamanhoPagina=500`;
   }
+
   if (!url) return { statusCode: 400, headers: {'Access-Control-Allow-Origin':'*'}, body: JSON.stringify({error:'Parâmetros inválidos'}) };
+
   try {
     const resp = await fetch(url, { headers: { 'Accept': 'application/json' } });
     const data = await resp.text();
