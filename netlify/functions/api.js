3ea44b1cac0a2acfea6db42f588941d66249379f
paulamatosoadv-bit <paulamatosoadv@gmail.com>
2026-08-10 11:45:07 -0300

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,5 @@
-// v2 exports.handler = async function(event) {
+// v2 
+exports.handler = async function(event) {
   const qs = event.queryStringParameters || {};
   const cnpj = qs.cnpj || '';
   const ano = qs.ano || '';
